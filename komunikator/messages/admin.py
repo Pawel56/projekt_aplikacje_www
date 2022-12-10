65d367225c06c870184168015a4b0d4d97c29a98
Paweł Janicki <pawel.jan526@gmail.com>
2022-12-10 06:12:40 +0100

    zmiana daty w modelu i dodanie wyświetlania w panelu admin

--- komunikator/messages/admin.py
+++ komunikator/messages/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Message
+from api.models import User
+
+class MessageAdmin(admin.ModelAdmin):
+        list_display = ['from_id', 'to_id', 'message', 'data_dodania']
+        list_filter = ('from_id', 'to_id', 'data_dodania',)
+
+admin.site.register(Message, MessageAdmin)
